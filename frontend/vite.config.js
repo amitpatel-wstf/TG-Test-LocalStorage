734af8373885baf86cf3a104562f06dc0020b200
amitpatel-wstf <amit.patel@thewasserstoff.com>
2025-08-21 19:21:35 +0530

    added frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        allowedHosts: ['68b17eb60f6d.ngrok-free.app'],
+    }
+    // run that app in the 5174
+    // run that app in the 5174 port
+    // run that app in the 5174 port
+});
+//# sourceMappingURL=vite.config.js.map
